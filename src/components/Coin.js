e4edef1e4e5618cc4008b96254be0f8bcc950ab3
tomyferra <ferra.tomy@gmail.com>
2023-02-07 15:49:41 -0300

    Converted thousands numbers to be in US format

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -19,13 +19,17 @@ function Coin ({ name, ticker,id, icon, dataFromParent, bitcoinAmount}) {
   catch (e) {
   }
 
+  let numberToDisplay=(btcPrice*bitcoinAmount/coinData)
+  numberToDisplay=numberToDisplay.toLocaleString('en-US', {maximumFractionDigits: 2})
+  console.log(numberToDisplay)
+
   return(
     <div className="coin-style h-40">
       <div className="container h-40">
         <div className="card-body border rounded row">
           <h5 className="col text-center card-title">{name} <i className={icon}></i></h5>
           <div className="col coin-value">            
-            {ticker==='BUSDUSDT' ? "$" +(btcPrice*bitcoinAmount/coinData).toFixed(2) : (btcPrice*bitcoinAmount/coinData).toFixed(2)}
+            {ticker==='BUSDUSDT' ? "$" +numberToDisplay : numberToDisplay}
           </div>
         </div>
       </div>
